856f99d25ee5ca1b5295bfd277ff8953aec905aa
wajahat-minhal <wajahatminhal200@gmail.com>
2024-09-29 20:59:42 +0500

    UI Created

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ var num;
 function toss(para) {
     num = Math.ceil(Math.random(num) * 2)
     if (para === 1 && num === 1 || para === 2 && num === 2) {
-        result.innerHTML = "The Winner Is Heads!"
+        result.innerHTML = "You Win The Toss!"
         console.log("You Win The Toss!" + num);
 
     } else {
         console.log("You Loss The Toss!" + num);
-        result.innerHTML = "The Winner Is Tails!"
+        result.innerHTML = "You Loss The Toss!"
     }
+}
+
+if (num === 1){
+    console.log("Its a Head")
+} else if(num === 2){
+    console.log("Its a Tail")
 }
